838ca1617105fc039c8ebd464422b6d65a1236b5
Sharmin Moon <sharminmoon0308@gmail.com>
2022-09-28 22:41:26 +0600

    project initialiazed with fake data

---
+++ src/fakeData/fakeStudyData.js
@@ -0,0 +1,54 @@
+const studyData = [
+  {
+    id: 1,
+    titile: "Bangla",
+    description: "Bangla is our mother  tounge",
+    img: "",
+    class: "class-11",
+    duration: 30,
+  },
+
+  {
+    id: 2,
+    titile: "English",
+    description: "English  is a international language",
+    img: "",
+    class: "class-11",
+    duration: 35,
+  },
+  {
+    id: 3,
+    titile: "Mathematics",
+    description: "Everyone should learn basic mathematics",
+    img: "",
+    class: "class-11",
+    duration: 40,
+  },
+  {
+    id: 4,
+    titile: "Physics",
+    description: "Physics is a important subjeect for every science student",
+    img: "",
+    class: "class-11",
+    duration: 35,
+  },
+  {
+    id: 5,
+    titile: "Chemistry",
+    description: "Chemistry is a important subjeect for every science student",
+    img: "",
+    class: "class-11",
+    duration: 40,
+  },
+
+  {
+    id: 6,
+    titile: "Biology",
+    description: "Biology is a important subjeect for every science student",
+    img: "",
+    class: "class-11",
+    duration: 25,
+  },
+];
+
+export default studyData;
